cfe58a3de6163e21d6f5259e0ae36a16dbb2ee36
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2022-04-08 09:51:51 +0300

    Start App

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Mahmut MERCAN on 7.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
